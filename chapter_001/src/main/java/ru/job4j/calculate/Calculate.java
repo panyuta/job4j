7eaac1d1a9859fa390c31b458137c1564652dbfc
panyuta <tatyana.panyutina@gmail.com>
2019-05-12 19:45:58 +0300

    1.3 JavaDoc. Code convention [#138769]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate решение задачи части 001 урок 1.
+ * @author panyuta
+ * @since 12.05.2019
+ */
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author panyuta
+ * @version $1.0$
+ * @since 12.05.2019
+ */
+ 
+package ru.job4j.calculate;
