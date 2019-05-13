a2f60949b4ecbe23ced22448b05fd6a314f57a73
panyuta <tatyana.panyutina@gmail.com>
2019-05-13 18:57:05 +0300

    3.5. Стороны прямоугольника.[#138761]

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public int square(int p, int k) {
+        double h = p * k / (2 * (1 + k));
+        double w = h / k;
+        return ((int)(h * w));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+    @Test
+    public void p4k1s1() {
+        SqArea sq = new SqArea();
+        assertThat(sq.square(4, 1), is(1));
+    }
+
+    @Test
+    public void p6k2s2() {
+        SqArea sq = new SqArea();
+        assertThat(sq.square(6, 2), is(2));
+    }
+}
